61a49e90f4f636f86386a5f0e3cf03ef918cce67
K.A Bimsara Kaushal <bimsarakaushal2002@gmail.com>
2026-02-10 13:27:16 +0530

    arr.length = n veriable

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -5,7 +5,9 @@ public class InsertionSort {
     public static void main(String[] args) {
         int arr[] = {10, 5, 4, 3, 2, 1};
 
-        for (int i = 1; i < arr.length; i++) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
             int temp = arr[i];
             int j = i - 1;
 
@@ -17,7 +19,7 @@ public class InsertionSort {
         }
 
         // Print sorted array
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
     }
